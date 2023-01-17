e09e8e4b6741d0587d8b587ff8d45f0ef10343b6
Takashi Kawachi <tkawachi@gmail.com>
2023-01-17 19:16:51 +0900

    update

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,16 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+
+const is_dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-
+        paths: {
+                base: is_dev ? '' : '/sqlviz'
+        },
 	kit: {
 		adapter: adapter()
 	}
